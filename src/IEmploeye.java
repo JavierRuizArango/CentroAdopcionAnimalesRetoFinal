84468a33a9d74e5d0d92b2d9dc07736b680c0cbc
JavierRuizArango <javierruizarango@gmail.com>
2023-10-18 15:44:28 -0500

    son eliminadas esta interfaces Iempleado sera reemplazada por una clase normal que va ser proceso de adpcion y tendra un crudo con sus metodos staticos tambien se elimina la interfaz iREGISTREr por que el admin es el unico que puede registrar  (crear )empleado por ello en lugar de una interfaz se va utilizar un crud para animales y otro para empleados dentro de la clase empleado

--- src/IEmploeye.java
+++
@@ -1,9 +0,0 @@
-public interface IEmploeye {
-    public interface IEmployee {
-    public void createAdoptionProcess();
-    public void readAdoptionProcess();
-    public void updateAdoptionProcess();
-    public void deleteAdoptionProcess();
-
-    }
-}
--- src/IEmployee.java
+++
@@ -1,7 +0,0 @@
-public interface IEmployee {
-    public void createAdoptionProcess();
-    public void readAdoptionProcess();
-    public void updateAdoptionProcess();
-    public void deleteAdoptionProcess();
-
-}
