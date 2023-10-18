510403069757e9d28af4e062ddf1715648d342b8
JavierRuizArango <javierruizarango@gmail.com>
2023-10-18 15:38:40 -0500

    creando la clase proceso de adopcion con sus metodos estaticos que usaremos para  que las demas clases puedan interactuar con el proceso de adopcion

---
+++ src/AdoptionProcess.java
@@ -0,0 +1,25 @@
+public class AdoptionProcess {
+
+    String idProcess;
+    String Adopter;
+
+    String date;
+
+    String statusProcess;
+
+    public static void createAdoptionProcess() {
+        //pendiente
+    }
+
+
+    public static void readAdoptionProcess() {
+        //pendiente
+
+    }
+    public static void updateAdoptionProcess() {
+        //pendiente
+    }
+    public static void deleteAdoptionProcess() {
+        //pendiente
+    }
+}
