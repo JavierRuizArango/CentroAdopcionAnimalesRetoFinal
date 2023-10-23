7e7e76da7710d5dd8b2dbd7054723881fe453590
JavierRuizArango <javierruizarango@gmail.com>
2023-10-23 08:36:22 -0500

    se elimnan los get y set de empleado por que estas son clases hijas de usuario

--- src/Admin.java
+++ src/Admin.java
@@ -1,53 +1,5 @@
 public abstract class Admin extends User implements IRead {
-    private String name;
-    private String lastname;
-    private long cc;
-    private String email;
-    protected String userType;
 
-    public Admin() {
-        super();
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getLastname() {
-        return lastname;
-    }
-
-    public void setLastname(String lastname) {
-        this.lastname = lastname;
-    }
-
-    public long getCc() {
-        return cc;
-    }
-
-    public void setCc(long cc) {
-        this.cc = cc;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getUserType() {
-        return userType;
-    }
-
-    public void setUserType(String UserType) {
-        UserType = UserType;
-    }
 
     public void createAnimal() {
 
@@ -65,22 +17,6 @@ public abstract class Admin extends User implements IRead {
 
     }
 
-    public void createEmploee() {
-
-    }
-
-
-    public void readEmploee() {
-
-    }
-
-    public void upDateEmploee() {
-
-    }
-
-    public void deleteEmploee() {
-
-    }
 
 
 }
--- src/Employee.java
+++ src/Employee.java
@@ -1,76 +1,21 @@
 
 
 public abstract class Employee  extends User implements IRead{
-    private String name;
-    private String lastname;
-    private long cc;
-    private String email;
-
-    protected String Roll;
-
-    protected String usertype;
-
-    public Employee(String name, String lastname, long cc, String email) {
-        super();
-    }
-
-    @Override
-    public String getName() {
-        return name;
-    }
-
-    @Override
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    @Override
-    public String getLastname() {
-        return lastname;
-    }
-
-    @Override
-    public void setLastname(String lastname) {
-        this.lastname = lastname;
-    }
-
-    @Override
-    public long getCc() {
-        return cc;
-    }
-
-    @Override
-    public void setCc(long cc) {
-        this.cc = cc;
-    }
-
-    @Override
-    public String getEmail() {
-        return email;
-    }
-
-    @Override
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getRoll() {
-        return Roll;
-    }
-
-    public void setRoll(String roll) {
-        Roll = roll;
-    }
-
-    public String getUsertype() {
-        return usertype;
-    }
-
-    public void setUsertype(String userType) {
-        this.usertype = usertype;
+    public void createEmploee() {
+      //aqui instancio   createAdoptionProcess() {
     }
 
 
+    public void readEmploee() {
 
+    }
+
+    public void upDateEmploee() {
+
+    }
+
+    public void deleteEmploee() {
+
+    }
 
 }
