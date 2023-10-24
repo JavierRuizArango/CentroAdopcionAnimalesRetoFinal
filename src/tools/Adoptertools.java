13cecfa49a6f350c311ca636eccf2bf1cee055c7
felipeherrerapere03 <felipeherrerapere03@gmail.com>
2023-10-24 17:58:03 -0500

    Arreglando el error del ciclo do while agregandole una condicion.

--- src/tools/Adoptertools.java
+++ src/tools/Adoptertools.java
@@ -18,15 +18,35 @@ public class Adoptertools {
             switch (respuestaSubMenu){
             case "1":
                 System.out.println("aqui le mostramos al adoptante los animales disponibles");
+                System.out.println("¿Desea realizar otra acción? (Sí/No)");
+                String otraAccion1 = scanner.nextLine().toLowerCase();
+                if (otraAccion1.equals("no")) {
+                    regresarAlMenuPrincipal = true;
+                }
                 break;
             case "2":
                 System.out.println("aqui se  Registra como adoptante");
+                System.out.println("¿Desea realizar otra acción? (Sí/No)");
+                String otraAccion2 = scanner.nextLine().toLowerCase();
+                if (otraAccion2.equals("no")) {
+                    regresarAlMenuPrincipal = true;
+                }
                 break;
             case "3" :
                 System.out.println("aqui actualiza sus  datos personales");
+                System.out.println("¿Desea realizar otra acción? (Sí/No)");
+                String otraAccion3 = scanner.nextLine().toLowerCase();
+                if (otraAccion3.equals("no")) {
+                    regresarAlMenuPrincipal = true;
+                }
                 break;
             case "4" :
                 System.out.println("aqui  Solicita un proceso de adopcion");
+                System.out.println("¿Desea realizar otra acción? (Sí/No)");
+                String otraAccion4 = scanner.nextLine().toLowerCase();
+                if (otraAccion4.equals("no")) {
+                    regresarAlMenuPrincipal = true;
+                }
                 break;
             case "5":
                 System.out.println("con esta opcion Sale del sub menú");
