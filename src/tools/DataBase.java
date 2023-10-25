35388cce7566548e42956acc48b09259e70754c1
JavierRuizArango <javierruizarango@gmail.com>
2023-10-25 12:50:16 -0500

    haciendo las validaciones con el try para cuando no exista una hoja de excel se cree tambien Para que no se sobreescriban las filas

--- src/tools/DataBase.java
+++ src/tools/DataBase.java
@@ -1,35 +1,50 @@
 package tools;
 
-import animalProcess.Animal;
-import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class DataBase {
+    private Workbook workbook;
+    private Sheet sheet1;
 
-    Workbook workbook = new XSSFWorkbook();
-    // Creamos las hojas de cálculo
-    Sheet sheet1 = (workbook.getSheet("Animales") == null) ? workbook.createSheet("Animales") : workbook.getSheet("Animales");
-    Sheet sheet2 = (workbook.createSheet("Adoptantes") == null)  ? workbook.createSheet("Adoptantes") : workbook.getSheet("Adoptantes");
-    Sheet sheet3 = (workbook.createSheet("Empleados")== null)  ? workbook.createSheet("Empleados") : workbook.getSheet("Empleados");
-    Sheet sheet4 = (workbook.createSheet("Administradores")== null)  ? workbook.createSheet("Administradores") : workbook.getSheet("Administradores");
-    Sheet sheet5 = (workbook.createSheet("Proceso")== null)  ? workbook.createSheet("Proceso") : workbook.getSheet("Proceso");
-    Row row = sheet1.createRow(0);
-    public DataBase(){
-        Row row = sheet1.createRow(0);
-        String[] title = {"ID", "Nombre", "Edad", "Especie", "EstadoSalud", "Descripcion", "Estatus"};
-        for (int i = 0; i < title.length; i++) {
-            row.createCell(i).setCellValue(title[i]);
+    public DataBase() {
+        try {
+            File file = new File("Database.xlsx");
+            FileInputStream fileIN;
+            if (file.exists()) {
+                fileIN = new FileInputStream(file);
+                workbook = new XSSFWorkbook(fileIN);
+                fileIN.close();
+            } else {
+                workbook = new XSSFWorkbook();
+            }
+
+            // Crear la hoja de cálculo "Animales" si no existe
+            sheet1 = workbook.getSheet("Animales");
+            if (sheet1 == null) {
+                sheet1 = workbook.createSheet("Animales");
+                Row headerRow = sheet1.createRow(0);
+                String[] title = {"ID", "Nombre", "Edad", "Especie", "EstadoSalud", "Descripcion", "Estatus"};
+                for (int i = 0; i < title.length; i++) {
+                    headerRow.createCell(i).setCellValue(title[i]);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
-    public void createAnimal(int id, String name, String age, String specie ,String healStatus, String description ,boolean available ){
-        Row row = this.sheet1.createRow(1);
+    public void createAnimal(int id, String name, String age, String specie, String healStatus, String description, boolean available) {
+        int lastRowNum = sheet1.getLastRowNum();
+        Row row = sheet1.createRow(lastRowNum + 1);
+
         // Agregamos los datos del animal a la fila
         row.createCell(0).setCellValue(id);
         row.createCell(1).setCellValue(name);
@@ -39,26 +54,26 @@ public class DataBase {
         row.createCell(5).setCellValue(description);
         row.createCell(6).setCellValue(available);
 
-
-
-
         this.escribirArchivo();
-
-
     }
-    public void escribirArchivo (){
-        try{
-            FileOutputStream outputStream = new FileOutputStream("C:\\Data/Database.xlsx");
-            this.workbook.write(outputStream);
-        }catch (IOException e) {
+
+    public void escribirArchivo() {
+        try {
+            FileOutputStream outputStream = new FileOutputStream("Database.xlsx");
+            workbook.write(outputStream);
+            outputStream.close();
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
-    public static int generateId () {
+
+    public static int generateId() {
         return (int) (Math.random() * 1000);
     }
-
 }
+
+
+
 
 
 
